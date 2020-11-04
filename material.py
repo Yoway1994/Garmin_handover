756d62fa97ff4a61cbc97f31e62a19ddad974216
Hsu <hsuyoway@ad.garmin.com>
2020-11-04 13:32:26 +0800

    2020-11-04 add thinfilm modules

---
+++ material.py
@@ -0,0 +1,259 @@
+import os
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+from scipy.optimize import basinhopping, minimize
+
+class Non_Dispersion:
+    def __init__(self, n, k = 0):
+        self.n = n
+        self.k = k
+
+    def nvalues(self, wl):
+        n = self.n*np.ones(np.size(wl))
+        return n
+
+    def kvalues(self, wl):
+        k = self.k*np.ones(np.size(wl))
+        return k
+        
+    def nk(self, wl):
+        n = self.nvalues(wl)
+        k = self.kvalues(wl)
+        return n - 1j*k
+
+class Material:
+    def __init__(self, wl, n, k = 0, wl_k = None):
+        self.wl = wl
+        self.n = n
+        self.k = k*np.ones(np.size(wl_k))
+        self.wl_k = wl_k
+
+    def nvalues(self, wl):
+        f = interp1d(self.wl, self.n, kind = 'cubic') # 內差法
+        try:
+            return f(wl)
+        except:
+            return f(wl/1000)
+        
+    def kvalues(self, wl):
+        f = interp1d(self.wl_k, self.k, kind = 'cubic')
+        try:
+            return f(wl)
+        except:
+            return f(wl/1000)
+
+    def nk(self, wl):
+        return self.nvalues(wl) - 1j*self.kvalues(wl)
+
+class Sellmeier:
+    def __init__(self, theta, k = 0, wl_k = 0):
+        self.theta = theta
+        self.k = k
+        self.wl_k = wl_k
+
+    def nvalues(self, wl):
+        theta = self.theta
+        wl = wl/1000
+        n = theta[0] + theta[1]/(1-theta[2]/wl**2) + theta[3]/(1-theta[4]/wl**2) + theta[5]/(1-theta[6]/wl**2)
+        return n**.5
+
+    def kvalues(self, wl):
+        try:
+            f = interp1d(self.wl_k, self.k, kind = 'cubic')
+            return f(wl)
+        except:
+            return self.k*np.ones(np.size(wl))
+
+    def nk(self, wl):
+        return self.nvalues(wl) - 1j*self.kvalues(wl)
+
+class Sellmeier_Simple:
+    def __init__(self, ABC, k = 0, wl_k = 0):
+        self.theta = ABC
+        self.k = k
+        self.wl_k = wl_k
+
+    def nvalues(self, wl):
+        theta = self.theta
+        wl = wl/1000
+        n = theta[0] + theta[1]/(1-theta[2]/wl**2)
+        return n**.5
+
+    def kvalues(self, wl):
+        try:
+            f = interp1d(self.wl_k, self.k, kind = 'cubic')
+            return f(wl)
+        except:
+            return self.k*np.ones(np.size(wl))
+
+    def nk(self, wl):
+        return self.nvalues(wl) - 1j*self.kvalues(wl)
+
+class Cauchy:
+    def __init__(self, BC, k = 0, wl_k = 0):
+        self.theta = BC
+        self.k = k
+        self.wl_k = wl_k
+
+    def nvalues(self, wl):
+        theta = self.theta
+        wl = wl/1000
+        n = theta[0] + theta[1]/wl**2
+        return n
+
+    def kvalues(self, wl):
+        try:
+            f = interp1d(self.wl_k, self.k, kind = 'cubic')
+            return f(wl)
+        except:
+            return self.k*np.ones(np.size(wl))
+
+    def nk(self, wl):
+        return self.nvalues(wl) - 1j*self.kvalues(wl)
+
+class Curve_Dispersion:
+    def __init__(self, ABC, k = 0, wl_k = 0):
+        self.theta = ABC
+        self.k = k
+        self.wl_k = wl_k
+
+    def nvalues(self, wl):
+        theta = self.theta
+        wl = wl/1000
+        n = theta[0]*wl**2 + theta[1]*wl + theta[2]
+        return n
+
+    def kvalues(self, wl):
+        try:
+            f = interp1d(self.wl_k, self.k, kind = 'cubic')
+            return f(wl)
+        except:
+            return self.k*np.ones(np.size(wl))
+
+    def nk(self, wl):
+        return self.nvalues(wl) - 1j*self.kvalues(wl)  
+    
+class Chromatic_Dispersion:
+    def __init__(self, c_n, c_k, c_w, k = 0, wl_k = 0):
+        self.cn = c_n
+        self.ck = c_k
+        self.cw = c_w
+        self.k = k
+        self.wl_k = wl_k
+    
+    def nvalues(self, wl):
+        n = self.ck*(wl-self.cw)/1000 + self.cn
+        return n
+    
+    def kvalues(self, wl):
+        try:
+            f = interp1d(self.wl_k, self.k, kind = 'cubic')
+            return f(wl)
+        except:
+            return self.k*np.ones(np.size(wl))
+    
+    def nk(self, wl):
+        return self.nvalues(wl) - 1j*self.kvalues(wl)
+    
+def sellmeier_fitting(target_w, target_n, init = np.zeros(7), save = False, name = None):
+    wl = np.array(target_w)/1000
+    hypo = lambda theta: theta[0] + theta[1]/(1-theta[2]/wl**2) + theta[3]/(1-theta[4]/wl**2) + theta[5]/(1-theta[6]/wl**2)
+    fom = lambda theta: sum(abs(hypo(theta) - np.array(target_n)**2))
+    res = basinhopping(fom, init)
+    print(res.message[0])
+    if save: save_material(res.x, name)
+    return res.x
+
+### material_book ###
+global m_path, m_book
+m_path = './material/data.csv'
+m_book = './material/book.csv'
+
+def new_material_book():
+    os.mkdir('./material')
+    with open(m_path, 'w+'):
+        pd.DataFrame({}).to_csv(m_path)
+    with open(m_book, 'w+'):
+        pd.DataFrame({}).to_csv(m_book)
+    print('new_material_book')
+
+def open_book():
+    bk = pd.read_csv(m_book, index_col = 0)
+    print(bk.columns[0], bk[0:].values[0][0])
+
+def save_material(m, name):
+    cls_name = type(m).__name__
+    _save_book(name, cls_name)
+    if cls_name == 'Non_Dispersion':
+        _save_material_data(m.n, name, '_mono_n')
+        _save_material_data(m.k, name, '_mono_k')
+    elif cls_name == 'Material':
+        _save_material_data(m.n, name, '_n')
+        _save_material_data(m.k, name, '_k')
+        _save_material_data(m.wl, name, '_w')
+        _save_material_data(m.wl_k, name, '_wk')
+    elif cls_name == 'Sellmeier':
+        _save_material_data(m.theta, name, '_SE')
+        _save_material_data(m.k, name, '_k')
+        _save_material_data(m.wl_k, name, '_wk')
+    else:
+        print('material type undefined')
+
+def _save_book(name, cls_name, cover = False):
+    if os.path.exists(m_book):
+        bk = pd.read_csv(m_book)
+    else:
+        new_material_book()
+        bk = pd.read_csv(m_book)
+    if (name in bk.columns) and cover == False:
+        print('Data already exist, set cover = True to overwrite the previous data')
+    else:
+        _save_material_data(cls_name, name, '')
+
+def _save_material_data(m_data, m_name, ext):
+    suffix = ['_SE', '_w', '_n', '_wk', '_k', '_mono_n', '_mono_k']
+    data_name = m_name + ext
+    if ext in suffix:
+        _save(m_data, data_name, m_path)
+    else:
+        _save([m_data], data_name, m_book)
+        
+def _save(m_data, data_name, m_path):
+    name = data_name
+    if os.path.exists(m_path):
+        file = pd.read_csv(m_path, index_col=0)
+        if name in file.columns:
+            file[name] = pd.Series(m_data)
+        else:
+            data = pd.DataFrame({name:m_data})
+            new_file = pd.concat([file, data], axis = 1)
+        new_file.to_csv(m_path)
+        print('{} has saved successfully'.format(m_path))
+    else:
+        new_material_book(m_path)
+
+def load_material_all():
+    bk = pd.read_csv('./material/book.csv')
+    
+def open_material(m_name, object_kind = None):
+    m_file = pd.read_csv(m_path, index_col=0)
+    if object_kind == 'Material':
+        m = Material(nan_remover(m_file[m_name+'_w']), nan_remover(m_file[m_name+'_n']))
+    elif object_kind == 'Sellmeier':
+        m = Sellmeier(nan_remover(m_file[m_name+'_SE']))
+    elif object_kind == 'Non_Dispersion':
+        m = Non_Dispersion(nan_remover(m_file[m_name+'_mono_n']), nan_remover(m_file[m_name+'_mono_k']))
+    else:
+        print('material database not founded')
+    try:
+        m.wl_k = nan_remover(m_file[m_name+'_wk'].values)
+        m.k = nan_remover(m_file[m_name+'_k'].values)
+    except:
+        m.wl_k = nan_remover(m_file[m_name+'_w'].values)
+        m.k = nan_remover(m_file[m_name+'_k'].values)
+    finally:
+        return m
+
+def nan_remover(v):
+    return [x for x in v if str(x) != 'nan']
---
+++ thinfilm.py
@@ -0,0 +1,186 @@
+import numpy as np
+import pandas as pd
+import copy
+from numpy import pi, sin, cos, arcsin, dot
+
+class Design:
+    def __init__(self, material, thickness):
+        layer = list(map(list, zip(material, thickness)))
+        self.ambient = layer[0][0]
+        self.substrate = layer[-1][0]
+        self.middle = layer[1:-1]
+    
+    def matrix(self, layer, wl, angle, pl):
+        if angle != 0:
+            theta_n = arcsin(self.ambient.nvalues(wl)*sin(angle)/layer[0].nvalues(wl))
+            ita = angular_dispersion(layer, wl, angle, pl, theta_n)
+            delta = 2*pi*layer[0].nvalues(wl)*layer[1]*cos(theta_n)/wl
+        else:
+            ita = layer[0].nk(wl)
+            delta = 2*pi*ita*layer[1]/wl
+        element = matrix_element(ita, delta)
+        return np.reshape(element.values.reshape(1,-1), (np.size(wl), 2, 2))
+    
+    def eq_tf_matrix(self, wl, angle, pl):
+        eq_matrix = identity_matrix(wl)
+        for layer in self.middle[::-1]:
+            eq_matrix = matrix_dot(self.matrix(layer, wl, angle, pl), eq_matrix)
+        return eq_matrix
+
+    def eq_admittance(self, wl, angle, pl):
+        eq_matrix = self.eq_tf_matrix(wl, angle, pl)
+        theta_s = arcsin(self.ambient.nvalues(wl)/self.substrate.nvalues(wl)*sin(angle))
+        if pl == "S":
+            eq_y = bc(eq_matrix, self.substrate.nk(wl)*cos(theta_s)/cos(angle), wl)
+        elif pl == "P":
+            eq_y = bc(eq_matrix, self.substrate.nk(wl)*cos(angle)/cos(theta_s), wl)
+        else:
+            eq_y = bc(eq_matrix, self.substrate.nk(wl), wl)
+        return eq_y
+    
+    def eq_Y(self, wl, angle, pl, rt):
+        eq_y = self.eq_admittance(wl, angle, pl)
+        if rt == "T":
+            return eq_y
+        elif rt == "R":
+            Y = eq_y['C']/eq_y['B']
+            return Y.values.reshape(np.size(wl), 1)
+    
+    def _transmittance(self, wl, angle, pl):
+        eq_y = self.eq_Y(wl, angle, pl, "T")
+        t = (self.ambient.nk(wl)*eq_y["B"]+eq_y["C"]).values
+        T = 4*self.ambient.nk(wl)*np.real(self.substrate.nk(wl))/(t*t.conjugate())
+        return T.real
+
+    def transmittance(self, wl, angle = 0, pl = None):
+        if angle != 0:
+            angle = angle*pi/180
+            if pl == "S":
+                return self._transmittance(wl, angle, "S")
+            elif pl == "P":
+                return self._transmittance(wl, angle, "P")
+            else:
+                return (self._transmittance(wl, angle, "S")+self._transmittance(wl, angle, "P"))/2
+        else:
+            return self._transmittance(wl, angle, pl)
+    
+    def _reflectance(self, wl, angle, pl):
+        eq_Y = self.eq_Y(wl, angle, pl, "R").flatten()
+        r = (self.ambient.nk(wl)-eq_Y)/(self.ambient.nk(wl)+eq_Y)
+        reflectance = np.reshape(r*r.conjugate(), np.size(eq_Y))
+        return np.real(reflectance)   
+        
+    def reflectance(self, wl, angle = 0, pl = None):
+        if angle != 0:
+            angle = angle*pi/180
+            if pl == "S":
+                return self._reflectance(wl, angle, "S")
+            elif pl == "P":
+                return self._reflectance(wl, angle, "P")
+            else:
+                return (self._reflectance(wl, angle, "S")+self._reflectance(wl, angle, "P"))/2
+        else:
+            return self._reflectance(wl, angle, pl)
+    
+    def _reflec_trans(self, wl, angle, pl):
+        eq_y = self.eq_admittance(wl, angle, pl)
+        # transmittance part
+        t = (self.ambient.nk(wl)*eq_y["B"]+eq_y["C"]).values
+        T = 4*self.ambient.nk(wl)*np.real(self.substrate.nk(wl))/(t*t.conjugate()) 
+        # reflectance
+        Y = eq_y['C']/eq_y['B']
+        eq_Y = Y.values.reshape(np.size(wl), 1).flatten()
+        r = (self.ambient.nk(wl)-eq_Y)/(self.ambient.nk(wl)+eq_Y)
+        R = np.reshape(r*r.conjugate(), np.size(eq_Y))
+        ###
+        return [np.real(R), T.real]
+    
+    def reflec_trans(self, wl, angle = 0, pl = None):
+        if angle != 0:
+            angle = angle*pi/180
+            if pl == "S":
+                return self._reflec_trans(wl, angle, "S")
+            elif pl == "P":
+                return self._reflec_trans(wl, angle, "P")
+            else:
+                S = self._reflec_trans(wl, angle, "S")
+                P = self._reflec_trans(wl, angle, "P")
+                return (S[0]+P[0])/2, (S[1]+P[1])/2
+        else:
+            return self._reflec_trans(wl, angle, pl)
+        
+def angular_dispersion(layer, wl, theta_0, pl, theta_n):
+    if pl == "S":
+            return layer[0].nk(wl)*cos(theta_n)/cos(theta_0)
+    elif pl == "P":
+            return layer[0].nk(wl)*cos(theta_0)/cos(theta_n)
+
+def identity_matrix(wl):
+    m = np.size(wl)
+    i = pd.DataFrame({'e1':np.ones(m), 'e2':np.zeros(m), 'e3':np.zeros(m), 'e4':np.ones(m)})
+    i_matrix = np.reshape(i.values.reshape(1,-1), (m, 2, 2))
+    return i_matrix
+
+def matrix_element(ita, delta):
+    e = pd.DataFrame(
+        {'e1':cos(delta), 'e2':1j/ita*sin(delta), 
+         'e3':1j*ita*sin(delta), 'e4':cos(delta)}
+    )
+    return e
+
+def matrix_dot(layer_up, layer_bot): 
+    w, _, _ = np.shape(layer_up)
+    eq = [dot(layer_up[i], layer_bot[i]) for i in range(w)]
+    return eq
+
+def bc(eq, ns, wl):
+    m = np.size(wl)
+    ita_s = np.reshape(pd.DataFrame({"one":np.ones(m),"ita":ns}).values.reshape(-1, 1), (m, 2, 1))
+    YY = [dot(eq[i], ita_s[i]) for i in range(m)]
+    bc = pd.DataFrame(np.reshape(YY, (m,2)), columns = ['B','C'])
+    return bc
+
+def margin(model, tol, wl):
+    margin_test = copy.deepcopy(model)
+    layer_margin = []
+    for i, layer in enumerate(margin_test.middle):
+        init_d = layer[-1]
+        margin = []
+        for j in np.linspace(-tol, tol, 20*tol+1):
+            margin_test.middle[i][-1] = init_d+j
+            margin.append(np.mean(margin_test.reflectance(wl)))
+        layer_margin.append([max(margin), min(margin), max(margin)-min(margin)])
+        margin_test.middle[i][-1] = init_d
+    return layer_margin[::-1]
+
+def sec_reflec(model, wl, angle = 0, pl = None):
+    # copy
+    D_model = copy.deepcopy(model)
+    D_model.substrate = model.ambient
+    D_model.ambient = model.substrate
+    D_model.middle = model.middle[::-1]
+    ###
+    #angle_s = arcsin(model.ambient.nvalues(w)/model.substrate.nvalues(w)*sin(angle*np.pi/180))
+    RB = Design([model.substrate, model.ambient],[None, None]).reflectance(wl)
+    RaD, TaD = D_model.reflec_trans(wl)
+    RaU, TaU = model.reflec_trans(wl)
+    R_w = (RaU + RB*(TaU*TaD-RaU*RaD))/(1-RaD*RB)
+    return R_w
+
+def sec_transmit(model, wl, angle = 0, pl = None):
+    BG = Design([model.substrate, model.ambient],[None, None])
+    #
+    D_model = copy.deepcopy(model)
+    D_model.substrate = model.ambient
+    D_model.ambient = model.substrate
+    D_model.middle = model.middle[::-1]
+    #
+    #angle_s = arcsin(model.ambient.nvalues(w)/model.substrate.nvalues(w)*sin(angle*np.pi/180))
+    RaD = D_model.reflectance(wl)
+    RB, TB = BG.reflec_trans(wl)
+    TaU = model.transmittance(wl)
+    T = (TaU*TB)/(1-RaD*RB)
+    return T
+
+def RMSE(hypo, target):
+    return (sum((hypo-target)**2)/np.size(target))**.5
